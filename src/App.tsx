edde58f6704b65575a05eeadd1158ae2781d5643
Nikolas Burk <nikolas.burk@gmail.com>
2022-08-04 16:53:17 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+// src/App.tsx
+
+import UserDisplay from './components/UserDisplay'
+import { useQuery } from 'urql'
+import { GetUsersDocument } from './graphql/generated'
+
+function App() {
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
+
+  console.log(`Results: `, results)
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      {
+        results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
+      }
+    </div>
+  )
+}
+
+export default App
+
+// // src/App.tsx
+// // src/App.tsx
+
+// import { User } from './types'
+// import UserDisplay from './components/UserDisplay'
+// import { useQuery } from 'urql'
+// import { GetUsersDocument } from './graphql/generated'
+
+// // ...
+// // ...
+
+// function App() {
+//   const [results] = useQuery({
+//     query: GetUsersDocument
+//   })
+
+//   return (
+//     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+//       {
+//         users.map((user, i) => <UserDisplay user={user} key={i}/>)
+//       }
+//     </div>
+//   )
+// }
+
+// export default App
